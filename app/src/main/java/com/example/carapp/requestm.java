bed492ec86353cb8756f1f86003f872efe4eef7a
abdessamad <abdessamadsouilem1@gmail.com>
2021-08-15 00:25:31 +0100

    start on map section

--- app/src/main/java/com/example/carapp/requestm.java
+++ app/src/main/java/com/example/carapp/requestm.java
@@ -3,12 +3,14 @@ package com.example.carapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -25,7 +27,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class requestm extends AppCompatActivity {
+public class requestm extends FragmentActivity implements OnMapReadyCallback {
   MapView mapView;
   FusedLocationProviderClient fusedLocationProviderClient;
   GoogleMap map;
@@ -35,7 +37,7 @@ public class requestm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requestm);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.BottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.requestm);
@@ -58,12 +60,11 @@ public class requestm extends AppCompatActivity {
                 return false;
             }
         });
-        mapView = findViewById(R.id.mapView);
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+       mapFragment.getMapAsync(this);
    // check permission
         if(ActivityCompat.checkSelfPermission(requestm.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-            getCurrentLocation();
+
         }
         else {
             ActivityCompat.requestPermissions(requestm.this , new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
@@ -74,24 +75,13 @@ public class requestm extends AppCompatActivity {
 
     }
 
-    public void getCurrentLocation() {
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if(location != null){
-                    currentLat = location.getLatitude();
-                    currentLong = location.getLongitude();
-                    mapView.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            map = googleMap;
-                        }
-                    });
-                }
-            }
-        });
-    }
+
+
+    public void onMapReady(GoogleMap googleMap){
+        map = googleMap;
+        LatLng Abdo = new LatLng(31.619989,-8.064442);
+        map.addMarker(new MarkerOptions().position(Abdo).title("Your location"));
+   }
 
 
 
